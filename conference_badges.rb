c1b01a598ae909b78f78f0655aa5b8c08870edfb
+ <+>
2022-10-21 18:40:44 +0300

    Completed assignment

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator arry
+    arry.map {|name| badge_maker(name)}
+end
+
+def assign_rooms arry
+    arry.map.with_index {|name, index| "Hello, #{name}! You'll be assigned to room #{index + 1}!"}
+end
+
+def printer arry
+    arry.map {|name| puts badge_maker(name)}
+    arry.map.with_index {|name, index| puts "Hello, #{name}! You'll be assigned to room #{index + 1}!"}
+end
